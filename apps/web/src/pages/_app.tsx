0b8dc20b8f86c2abc22a156d29e4e2d4ef5d34df
Hugo Pineda <hugo@zumper.com>
2022-06-12 11:39:07 -0700

    added roboto font

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,13 +3,14 @@ import { Global } from '@emotion/react'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { theme, globalStyle } from 'theme'
+import '../fonts/roboto/roboto.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <title>Turbo/Next/Chakra App</title>
+        <title>Next.JS/Chakra-UI Starter App</title>
       </Head>
       <Global styles={globalStyle} />
       <Component {...pageProps} />
--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Heading } from '@chakra-ui/react'
+
 export default function Index() {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <Heading>Hello, World</Heading>
     </div>
   )
 }
